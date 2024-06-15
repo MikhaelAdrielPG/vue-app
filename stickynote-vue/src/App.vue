e96f0dc21e99f9531b070818dae9723c04c25c20
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-06-15 09:41:17 +0700

    Membuat Event Handler Untuk Menyimpan Data

--- stickynote-vue/src/App.vue
+++ stickynote-vue/src/App.vue
@@ -2,10 +2,27 @@
 import { ref } from "vue";
 const showForm = ref(false);
 const newMemo = ref("");
+const memos = ref([]);
+
+function addMemo() {
+  memos.value.push({
+    id: Date.now(),
+    memo: newMemo.value,
+    date: new Date().toLocaleDateString("en-GB"),
+    backgroundColor: getRandomColor(),
+  });
+  newMemo.value = "";
+  showForm.value = false;
+}
+
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 </script>
 
 <template>
   <main>
+    {{ memos }}
     <div class="container">
       <header>
         <h1 class="header-title">Memo</h1>
@@ -63,7 +80,7 @@ const newMemo = ref("");
           cols="30"
           rows="10"
         ></textarea>
-        <button class="form-save-btn">Save</button>
+        <button @click="addMemo" class="form-save-btn">Save</button>
       </div>
     </div>
   </main>

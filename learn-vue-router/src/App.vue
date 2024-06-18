df17bf2c44ae771d43f30d1536081be56b146b61
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-06-18 13:10:19 +0700

    Menggunakan Routerlink Untuk Berpindah Halaman

--- learn-vue-router/src/App.vue
+++ learn-vue-router/src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
 import { RouterView } from "vue-router";
+import Nav from "./components/Nav.vue";
+import { ref } from "vue";
+
+const count = ref(0);
 </script>
 
 <template>
+  <Nav />
+  <button @click="count++">count is: {{ count }}</button>
   <main>
     <RouterView />
   </main>

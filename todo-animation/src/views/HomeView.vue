9e0c0d4abed227a53b71c1a528da1f5d34954031
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-06-20 08:04:06 +0700

    Membuat Animasi Transisi Keluar Dengan Transitiongroup Dan Transisi Move

--- todo-animation/src/views/HomeView.vue
+++ todo-animation/src/views/HomeView.vue
@@ -1,73 +1,88 @@
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
 
-const tasks = ref(["Learn HTML", "Learn CSS", "Learn JavaScript"]);
-const newTask = ref("");
+const tasks = ref(['Learn HTML', 'Learn CSS', 'Learn JavaScript']);
+const newTask = ref('');
 
 function addTask() {
-  if (newTask.value) {
-    tasks.value.unshift(newTask.value);
-    newTask.value = "";
-  }
+	if (newTask.value) {
+		tasks.value.unshift(newTask.value);
+		newTask.value = '';
+	}
 }
 
-function removeTask(task) {
-  tasks.value = tasks.value.filter((t) => t !== task);
+function removeTask(index) {
+	tasks.value.splice(index, 1);
 }
 </script>
 
 <template>
-  <main>
-    <div class="container">
-      <input type="text" v-model="newTask" @keyup.enter="addTask()" autofocus />
-      <TransitionGroup name="list">
-        <div
-          class="card-list"
-          v-for="task in tasks"
-          :key="task"
-          @click="removeTask(task)"
-        >
-          {{ task }}
-        </div>
-      </TransitionGroup>
-    </div>
-  </main>
+	<main>
+		<div class="container">
+			<input type="text" v-model="newTask" @keyup.enter="addTask()" autofocus />
+			<TransitionGroup name="list">
+				<div
+					class="card-list"
+					v-for="task in tasks"
+					:key="task"
+					@click="removeTask(tasks.indexOf(task))"
+				>
+					{{ task }}
+				</div>
+			</TransitionGroup>
+		</div>
+	</main>
 </template>
 
 <style scoped>
 .container {
-  max-width: 300px;
-  margin: 0 auto;
+	max-width: 300px;
+	margin: 0 auto;
 }
 
 .container input {
-  width: 100%;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin-bottom: 20px;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+	width: 100%;
+	border-radius: 5px;
+	border: 1px solid #ccc;
+	padding: 10px;
+	margin-bottom: 20px;
+	box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
 }
 
 .card-list {
-  width: 300px;
-  border-radius: 5px;
-  padding: 5px 10px;
-  margin-top: 10px;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  text-align: center;
-  cursor: pointer;
+	width: 300px;
+	border-radius: 5px;
+	padding: 5px 10px;
+	margin-top: 10px;
+	box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+	text-align: center;
+	cursor: pointer;
 }
 
 .list-enter-from {
-  opacity: 0;
-  transform: scale(0.6);
+	opacity: 0;
+	transform: scale(0.6);
 }
 .list-enter-to {
-  opacity: 1;
-  transform: scale(1);
+	opacity: 1;
+	transform: scale(1);
 }
 .list-enter-active {
-  transition: all 0.5s ease;
+	transition: all 0.5s ease;
+}
+.list-leave-from {
+	opacity: 1;
+	transform: scale(1);
+}
+.list-leave-to {
+	opacity: 0;
+	transform: scale(0);
+}
+.list-leave-active {
+	transition: all 0.5s ease;
+	position: absolute;
+}
+.list-move {
+	transition: all 0.5s ease;
 }
 </style>
